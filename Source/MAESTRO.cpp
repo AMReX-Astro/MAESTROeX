e925ca3d6d566ba64b030f968df3552f8002f22e
Andy Nonaka <AJNonaka@lbl.gov>
2017-09-18 18:25:20 -0700

    clean up time stepping

--- Source/MAESTRO.cpp
+++ Source/MAESTRO.cpp
@@ -598,36 +598,44 @@ MAESTRO::Advance (int lev, Real time)
   }
 }
 
-// a wrapper for EstTimeStep
+// a wrapper for ComputeDtLevel
 void
 MAESTRO::ComputeDt ()
 {
+
+  // compute dt at each level from CFL considerations
   Array<Real> dt_tmp(finest_level+1);
-
   for (int lev = 0; lev <= finest_level; ++lev) {
-    dt_tmp[lev] = EstTimeStep(lev, true);
+    dt_tmp[lev] = ComputeDtLevel(lev);
   }
-  ParallelDescriptor::ReduceRealMin(&dt_tmp[0], dt_tmp.size());
 
-  constexpr Real change_max = 1.1;
+  // select the smallest time step over all levels
   Real dt_0 = dt_tmp[0];
-  for (int lev = 0; lev <= finest_level; ++lev) {
-    dt_tmp[lev] = std::min(dt_tmp[lev], change_max*dt);
+  for (int lev = 1; lev <= finest_level; ++lev) {
     dt_0 = std::min(dt_0, dt_tmp[lev]);
+  }
+
+  // do not allow time step to increase too much from previous
+  constexpr Real change_max = 1.1;
+  if (dt_0 > change_max*dt)
+  {
+    amrex::Print() << "Reducing time step to respect change_max" << std::endl;
+    dt_0 = change_max*dt;
   }
 
   // Limit dt's by the value of stop_time.
   const Real eps = 1.e-3*dt_0;
   if (t_new[0] + dt_0 > stop_time - eps) {
+    amrex::Print() << "Modifying time step to respect stop_time" << std::endl;
     dt_0 = stop_time - t_new[0];
   }
 
   dt = dt_0;
 }
 
-// compute dt from CFL considerations
+// compute dt at a level from CFL considerations
 Real
-MAESTRO::EstTimeStep (int lev, bool local) const
+MAESTRO::ComputeDtLevel (int lev) const
 {
   BL_PROFILE("MAESTRO::EstTimeStep()");
 
@@ -666,9 +674,7 @@ MAESTRO::EstTimeStep (int lev, bool local) const
     }
   }
 
-  if (!local) {
-    ParallelDescriptor::ReduceRealMin(dt_est);
-  }
+  ParallelDescriptor::ReduceRealMin(dt_est);
 
   dt_est *= cfl;
 
