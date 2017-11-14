b85296d1a7623031cca94a09f47dd742207d51a3
Andy Nonaka <AJNonaka@lbl.gov>
2017-11-13 16:06:56 -0800

    make_S_cc()

--- Source/MaestroMakeS.cpp
+++ Source/MaestroMakeS.cpp
@@ -4,34 +4,70 @@
 using namespace amrex;
 
 void
-Maestro::Make_S_cc (Vector<std::unique_ptr<MultiFab> >& S_cc)
+Maestro::Make_S_cc (Vector<std::unique_ptr<MultiFab> >& S_cc,
+                    Vector<std::unique_ptr<MultiFab> >& scal,
+                    Vector<std::unique_ptr<MultiFab> >& rho_omegadot,
+                    Vector<std::unique_ptr<MultiFab> >& rho_Hnuc,
+                    Vector<std::unique_ptr<MultiFab> >& rho_Hext,
+                    Vector<std::unique_ptr<MultiFab> >& thermal)
 {
 
     for (int lev=0; lev<=finest_level; ++lev) 
     {
         MultiFab& S_cc_mf = *S_cc[lev];
+        MultiFab& scal_mf = *scal[lev];
+        MultiFab& rho_odot_mf = *rho_omegadot[lev];
+        MultiFab& rho_Hnuc_mf = *rho_Hnuc[lev];
+        MultiFab& rho_Hext_mf = *rho_Hext[lev];
+        MultiFab& thermal_mf = *thermal[lev];
 
         for ( MFIter mfi(S_cc_mf); mfi.isValid(); ++mfi ) // Loop over boxes
         {
-            // Get the index space of this iteration
-            const Box& box = mfi.validbox();
+            // get references to the FABs, each containing data and the valid+ghost box
+            FArrayBox& S_cc_fab = S_cc_mf[mfi];
+            FArrayBox& scal_fab = scal_mf[mfi];
+            FArrayBox& rho_odot_fab = rho_odot_mf[mfi];
+            FArrayBox& rho_Hnuc_fab = rho_Hnuc_mf[mfi];
+            FArrayBox& rho_Hext_fab = rho_Hext_mf[mfi];
+            FArrayBox& thermal_fab = thermal_mf[mfi];
 
-            // Get a reference to the FAB , which contains data and box
-            FArrayBox& fab = S_cc_mf[mfi];
+            // Get the index space of the valid region
+            const Box& validBox = mfi.validbox();
 
-            // Get the index space for the data region in the FAB.
-            // Note "abox" may have ghost cells, and is thus larger than
-            // or equal to "box" obtained using mfi.validbox().
-            const Box & abox = fab.box();
+            // Get the index space of the valid+ghost region for each FAB
+            // Note each of these boxes may contain ghost cells, and thus are
+            // larger than or equal to mfi.validbox().
+            const Box & S_cc_box = S_cc_fab.box();
+            const Box & scal_box = scal_fab.box();
+            const Box & rho_odot_box = rho_odot_fab.box();
+            const Box & rho_Hnuc_box = rho_Hnuc_fab.box();
+            const Box & rho_Hext_box = rho_Hext_fab.box();
+            const Box & thermal_box = thermal_fab.box();
 
             // We can now pass the information to a Fortran routine,
-            // fab.dataPtr() gives a double*, which is reshaped into
+            // e.g. S_cc_fab.dataPtr() gives a double*, which is reshaped into
             // a multi-dimensional array with dimensions specified by
-            // the information in "abox". We will also pass "box",
+            // the information in "S_cc_box". We will also pass "box",
             // which specifies our "work" region .
-            make_S_cc(ARLIM_3D(box.loVect()),ARLIM_3D(box.hiVect()),
-                      fab.dataPtr(), ARLIM_3D(abox.loVect()),ARLIM_3D(abox.hiVect()),
-                      S_cc_mf.nGrow(), fab.nComp());
+            make_S_cc(ARLIM_3D(validBox.loVect()), ARLIM_3D(validBox.hiVect()),
+                      S_cc_fab.dataPtr(),
+                      ARLIM_3D(S_cc_box.loVect()), ARLIM_3D(S_cc_box.hiVect()),
+                      S_cc_fab.nComp(),
+                      scal_fab.dataPtr(),
+                      ARLIM_3D(scal_box.loVect()), ARLIM_3D(scal_box.hiVect()),
+                      scal_fab.nComp(),
+                      rho_odot_fab.dataPtr(),
+                      ARLIM_3D(rho_odot_box.loVect()), ARLIM_3D(rho_odot_box.hiVect()),
+                      rho_odot_fab.nComp(),
+                      rho_Hnuc_fab.dataPtr(),
+                      ARLIM_3D(rho_Hnuc_box.loVect()), ARLIM_3D(rho_Hnuc_box.hiVect()),
+                      rho_Hnuc_fab.nComp(),
+                      rho_Hext_fab.dataPtr(),
+                      ARLIM_3D(rho_Hext_box.loVect()), ARLIM_3D(rho_Hext_box.hiVect()),
+                      rho_Hext_fab.nComp(),
+                      thermal_fab.dataPtr(),
+                      ARLIM_3D(thermal_box.loVect()), ARLIM_3D(thermal_box.hiVect()),
+                      thermal_fab.nComp());
         }
 
     }
--- Source/Src_nd/make_S.f90
+++ Source/Src_nd/make_S.f90
@@ -1,33 +1,68 @@
-subroutine make_S_cc(lo, hi,S_cc, s_lo, s_hi, &
-                     ng, ncomp) bind (C,name="make_S_cc")
+subroutine make_S_cc(lo, hi, &
+                     S_cc,  s_lo, s_hi, nc_s, &
+                     scal,  c_lo, c_hi, nc_c, &
+                     rodot, r_lo, r_hi, nc_r, &
+                     rHnuc, n_lo, n_hi, nc_n, &
+                     rHext, e_lo, e_hi, nc_e, &
+                     therm, t_lo, t_hi, nc_t) bind (C,name="make_S_cc")
 
   use eos_type_module
   use eos_module
   use network, only: nspec
+  use meth_params_module, only: rho_comp, temp_comp, spec_comp
 
-  integer         , intent (in   ) :: lo(3), hi(3), s_lo(3), s_hi(3), ng, ncomp
-  double precision, intent (inout) :: S_cc(s_lo(1):s_hi(1), &
-                                           s_lo(2):s_hi(2), &
-                                           s_lo(3):s_hi(3),1)
+  integer         , intent (in   ) :: lo(3), hi(3)
+  integer         , intent (in   ) :: s_lo(3), s_hi(3), nc_s
+  integer         , intent (in   ) :: c_lo(3), c_hi(3), nc_c
+  integer         , intent (in   ) :: r_lo(3), r_hi(3), nc_r
+  integer         , intent (in   ) :: n_lo(3), n_hi(3), nc_n
+  integer         , intent (in   ) :: e_lo(3), e_hi(3), nc_e
+  integer         , intent (in   ) :: t_lo(3), t_hi(3), nc_t
+  double precision, intent (inout) :: S_cc (s_lo(1):s_hi(1),s_lo(2):s_hi(2),s_lo(3):s_hi(3))
+  double precision, intent (inout) :: scal (c_lo(1):c_hi(1),c_lo(2):c_hi(2),c_lo(3):c_hi(3),nc_c)
+  double precision, intent (inout) :: rodot(r_lo(1):r_hi(1),r_lo(2):r_hi(2),r_lo(3):r_hi(3),nc_r)
+  double precision, intent (inout) :: rHnuc(n_lo(1):n_hi(1),n_lo(2):n_hi(2),n_lo(3):n_hi(3))
+  double precision, intent (inout) :: rHext(e_lo(1):e_hi(1),e_lo(2):e_hi(2),e_lo(3):e_hi(3))
+  double precision, intent (inout) :: therm(t_lo(1):t_hi(1),t_lo(2):t_hi(2),t_lo(3):t_hi(3))
 
   integer i,j,k
-  integer :: pt_index(3)
+  integer pt_index(3)
   type(eos_t) :: eos_state
+
+  integer comp
+  double precision sigma, xi_term, pres_term
 
   ! loop over the data
   do k = lo(3),hi(3)
   do j = lo(2),hi(2)
   do i = lo(1),hi(1)
+     
+     eos_state%rho   = scal(i,j,k,rho_comp)
+     eos_state%T     = scal(i,j,k,temp_comp)
+     eos_state%xn(:) = scal(i,j,k,spec_comp:spec_comp+nspec-1)/eos_state%rho
 
-     eos_state%rho   = 1.d0
-     eos_state%T     = 300.d0
-     eos_state%xn(1:nspec-1) = 0.01d0
-     eos_state%xn(nspec) = 1- sum(eos_state%xn(1:nspec-1))
+     pt_index(:) = (/i, j, k/)
 
      ! dens, temp, and xmass are inputs
-     call eos(eos_input_rt, eos_state)
+     call eos(eos_input_rt, eos_state, pt_index)
 
-     S_cc(i,j,k,1) = 0.d0
+     sigma = eos_state%dpdt / &
+          (eos_state%rho * eos_state%cp * eos_state%dpdr)
+
+     xi_term = 0.d0
+     pres_term = 0.d0
+     do comp = 1, nspec
+        xi_term = xi_term - &
+             eos_state%dhdX(comp)*rodot(i,j,k,comp)/eos_state%rho 
+
+        pres_term = pres_term + &
+             eos_state%dpdX(comp)*rodot(i,j,k,comp)/eos_state%rho
+     enddo
+
+     S_cc(i,j,k) = (sigma/eos_state%rho) * &
+          ( rHext(i,j,k) + rHnuc(i,j,k) + therm(i,j,k) ) &
+          + sigma*xi_term &
+          + pres_term/(eos_state%rho*eos_state%dpdr)
 
   enddo
   enddo
