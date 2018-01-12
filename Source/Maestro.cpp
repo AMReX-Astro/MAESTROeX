f5a3dc842d601362fe8690caec38a41924bf7132
Andy Nonaka <AJNonaka@lbl.gov>
2018-01-12 08:10:55 -0800

    move solver tolerance parameters to _cpp_parameters so we can control via inputs file

--- Source/Maestro.cpp
+++ Source/Maestro.cpp
@@ -21,28 +21,6 @@ int Maestro::divu_iters_comp         = 2;
 int Maestro::pressure_iters_comp     = 3;
 int Maestro::regular_timestep_comp   = 4;
 
-//////////////////////////////////
-// solver tolerances
-
-// tolerances for the initial projection
-Real Maestro::eps_init_proj_cart = 1.e-12;
-Real Maestro::eps_init_proj_sph  = 1.e-10;
-// tolerances for the divu iterations
-Real Maestro::eps_divu_cart = 1.e-12;
-Real Maestro::eps_divu_sph  = 1.e-10;
-Real Maestro::divu_iter_factor = 100.;
-Real Maestro::divu_level_factor = 10.;
-// tolerances for the MAC projection
-Real Maestro::eps_mac = 1.e-10;
-Real Maestro::eps_mac_max = 1.e-8;
-Real Maestro::mac_level_factor = 10.;
-Real Maestro::eps_mac_bottom = 1.e-3;
-// tolerances for the nodal projection
-Real Maestro::eps_hg = 1.e-12;
-Real Maestro::eps_hg_max = 1.e-10;
-Real Maestro::hg_level_factor = 10.;
-Real Maestro::eps_hg_bottom = 1.e-4;
-
 // helper IntVects used to define face/nodal MultiFabs
 #if (AMREX_SPACEDIM == 2)
 IntVect Maestro::nodal_flag(1,1);
