fd74f3e6854a481371abb2a8c6fbe837368531fc
Andy Nonaka <AJNonaka@lbl.gov>
2018-02-05 14:30:21 -0800

    fix the logic on the relative multigrid tolerance calculation to match the fortran maestro code

--- Source/MaestroNodalProj.cpp
+++ Source/MaestroNodalProj.cpp
@@ -247,15 +247,15 @@ Maestro::NodalProj (int proj_type,
         }
         else {
             if (istep_divu_iter == init_divu_iter) {
-                rel_tol = std::min(eps_divu_cart*pow(divu_level_factor,finest_radial_level), 
+                rel_tol = std::min(eps_divu_cart*pow(divu_level_factor,finest_level), 
                                    eps_divu_cart*pow(divu_level_factor,2));
             }
             else if (istep_divu_iter == init_divu_iter-1) {
-                rel_tol = std::min(eps_divu_cart*divu_iter_factor*pow(divu_level_factor,finest_radial_level),
+                rel_tol = std::min(eps_divu_cart*divu_iter_factor*pow(divu_level_factor,finest_level),
                                    eps_divu_cart*divu_iter_factor*pow(divu_level_factor,2));
             }
             else if (istep_divu_iter <= init_divu_iter-2) {
-                rel_tol = std::min(eps_divu_cart*pow(divu_iter_factor,2)*pow(divu_level_factor,finest_radial_level),
+                rel_tol = std::min(eps_divu_cart*pow(divu_iter_factor,2)*pow(divu_level_factor,finest_level),
                                    eps_divu_cart*pow(divu_iter_factor,2)*pow(divu_level_factor,2));
             }
         }
