4dc69e52efdcf87b76564d782892f91b3878c1e0
Alice Harpole <aliceharpole@gmail.com>
2019-01-16 10:24:32 -0500

    converted more of the varden_aux functions in test_projection

--- Exec/UNIT_TESTS/test_projection/MaestroEvolve.cpp
+++ Exec/UNIT_TESTS/test_projection/MaestroEvolve.cpp
@@ -1,5 +1,6 @@
 
 #include <Maestro.H>
+#include <Problem_F.H>
 
 using namespace amrex;
 
@@ -7,9 +8,202 @@ using namespace amrex;
 void
 Maestro::Evolve ()
 {
-    // timer for profiling
-    BL_PROFILE_VAR("Maestro::Evolve()",Evolve);
+	// timer for profiling
+	BL_PROFILE_VAR("Maestro::Evolve()",Evolve);
 
-    Print() << "Calling Evolve()" << std::endl;
+	Print() << "Calling Evolve()" << std::endl;
+
+	int project_type;
+	get_project_type(&project_type);
+
+	// need to make umid somewhere
+
+	if (project_type == 2) {
+		// MAC projection, need to initialize some more multifabs.
+		// init_mac_velocity
+	}
+
+	//--------------------------------------------------------------------------
+	// 'pollute' the velocity field by adding the gradient of a scalar
+	//--------------------------------------------------------------------------
+
+	if (project_type == 1) {
+		// add_grad_scalar
+
+		for (int lev=0; lev<=finest_level; ++lev) {
+			MultiFab& gphi_mf = gphi[lev];
+			MultiFab& umid_mf = umid[lev];
+			const Real* dx = geom[lev].CellSize();
+
+#ifdef _OPENMP
+#pragma omp parallel
+#endif
+			for (MFIter mfi(gphi_mf), true); mfi.isValid(); ++mfi)
+			{
+				const Box& tilebox = mfi.tilebox();
+				const int* lo  = tilebox.loVect();
+				const int* hi  = tilebox.hiVect();
+
+				add_grad_scalar(ARLIM_3D(lo), ARLIM_3D(hi),
+				                BL_TO_FORTRAN_FAB(gphi_mf[mfi]),
+				                BL_TO_FORTRAN_FAB(umid_mf[mfi]),
+				                phys_bc.dataPtr(), ZFILL(dx));
+
+			}
+		}
+
+		// copy umid to unew
+	} else {
+		for (int lev=0; lev<=finest_level; ++lev) {
+			MultiFab& gphi_mac_mf = gphi_mac[lev];
+			MultiFab& umac_mid_mf = umac_mid[lev];
+			const Real* dx = geom[lev].CellSize();
+#ifdef _OPENMP
+#pragma omp parallel
+#endif
+			for (MFIter mfi(gphi_mac_mf, true); mfi.isValid(); ++mfi)
+			{
+				const Box& tilebox = mfi.tilebox();
+				const int* lo  = tilebox.loVect();
+				const int* hi  = tilebox.hiVect();
+
+				// NOTE: I have no idea what 'box_phys_bc' is in MAESTROeX, so am
+				// guessing it's bcs_u[0]?
+
+				add_grad_scalar_mac(ARLIM_3D(lo), ARLIM_3D(hi),
+				                    BL_TO_FORTRAN_FAB(gphi_mac_mf[mfi]),
+				                    BL_TO_FORTRAN_FAB(umac_mid_mf[mfi]),
+				                    phys_bc.dataPtr(), bcs_u[0].data(), ZFILL(dx));
+
+			}
+		}
+
+		for (int lev=0; lev<=finest_level; ++lev) {
+			MultiFab& umac_mid_mf = umac_mid[lev];
+			MultiFab& utemp_mf = utemp[lev];
+			const Real* dx = geom[lev].CellSize();
+
+#ifdef _OPENMP
+#pragma omp parallel
+#endif
+			for (MFIter mfi(umac_mid_mf, true); mfi.isValid(); ++mfi)
+			{
+				const Box& tilebox = mfi.tilebox();
+				const int* lo  = tilebox.loVect();
+				const int* hi  = tilebox.hiVect();
+
+				convert_MAC_to_cc(ARLIM_3D(lo), ARLIM_3D(hi),
+				                  BL_TO_FORTRAN_FAB(umac_mid_mf[mfi]),
+				                  BL_TO_FORTRAN_FAB(utemp_mf[mfi]));
+
+			}
+		}
+
+        for (int lev=0; lev<=finest_level; ++lev) {
+			MultiFab& gphi_mac_mf = gphi_mac[lev];
+			MultiFab& utemp_mf = utemp[lev];
+			const Real* dx = geom[lev].CellSize();
+
+#ifdef _OPENMP
+#pragma omp parallel
+#endif
+			for (MFIter mfi(gphi_mac_mf, true); mfi.isValid(); ++mfi)
+			{
+				const Box& tilebox = mfi.tilebox();
+				const int* lo  = tilebox.loVect();
+				const int* hi  = tilebox.hiVect();
+
+				convert_MAC_to_cc(ARLIM_3D(lo), ARLIM_3D(hi),
+				                  BL_TO_FORTRAN_FAB(gphi_mac_mf[mfi]),
+				                  BL_TO_FORTRAN_FAB(utemp_mf[mfi]));
+
+			}
+		}
+
+		for (int lev=0; lev<=finest_level; ++lev) {
+			for (auto comp=0; comp < AMREX_SPACEDIM; ++comp)
+				MultiFab::Copy(umac_new[lev], umac_mid[lev], comp, comp, 1,
+				               umac_new[lev].nGrow());
+		}
+	}
+
+	//--------------------------------------------------------------------------
+	// project out the divergent portion of the velocity field
+	//--------------------------------------------------------------------------
+
+	if (project_type == 1) {
+		// hgprojection -- here pi is nodal and u is cell-centered
+
+		for (int lev=0; lev<=finest_level; ++lev) {
+			sold[lev].setVal(0.);
+			snew[lev].setVal(0.);
+			sold[lev].setVal(1., Rho, 1, 1);
+			snew[lev].setVal(1., Rho, 1, 1);
+			uold[lev].setVal(0.);
+			unew[lev].setVal(0.);
+			pi[lev].setVal(0.);
+			gpi[lev].setVal(0.);
+			rhcc_for_nodalproj[lev].setVal(0.);
+		}
+
+		std::fill(beta0_old.begin(), beta0_old.end(), 1.);
+		std::fill(beta0_nph.begin(), beta0_nph.end(), 1.);
+		std::fill(beta0_new.begin(), beta0_new.end(), 1.);
+
+		t_new = t_old + 1.;
+
+		// hgproject
+		NodalProj(initial_projection_comp, rhcc_for_nodalproj);
+
+	} else {
+		// mac projection -- here pi is cell-centered and u is MAC
+
+		Vector<MultiFab> rhohalf(finest_level+1);
+		Vector<MultiFab> macpi(finest_level+1);
+		Vector<MultiFab> macrhs(finest_level+1);
+
+		std::fill(beta0_old.begin(), beta0_old.end(), 1.);
+
+		for (int lev=0; lev<=finest_level; ++lev) {
+			// cell-centered MultiFabs
+			rhohalf[lev].define(grids[lev], dmap[lev], Nscal,    1);
+			rhohalf[lev].setVal(1.);
+			macpi[lev].define(grids[lev], dmap[lev],       1,    1);
+			macpi[lev].setVal(0.);
+			macrhs[lev].define(grids[lev], dmap[lev],       1,    1);
+			macrhs[lev].setVal(0.);
+
+			sold[lev].setVal(0.);
+			snew[lev].setVal(0.);
+			sold[lev].setVal(1., Rho, 1, 1);
+			snew[lev].setVal(1., Rho, 1, 1);
+		}
+
+		// macproject
+		auto is_predictor = 0;
+		MacProj(umac_new,macpi,macrhs,beta0_old,is_predictor);
+
+        for (int lev=0; lev<=finest_level; ++lev) {
+			MultiFab& umac_new_mf = umac_new[lev];
+			MultiFab& utemp_mf = utemp[lev];
+			const Real* dx = geom[lev].CellSize();
+
+#ifdef _OPENMP
+#pragma omp parallel
+#endif
+			for (MFIter mfi(umac_new_mf, true); mfi.isValid(); ++mfi)
+			{
+				const Box& tilebox = mfi.tilebox();
+				const int* lo  = tilebox.loVect();
+				const int* hi  = tilebox.hiVect();
+
+				convert_MAC_to_cc(ARLIM_3D(lo), ARLIM_3D(hi),
+				                  BL_TO_FORTRAN_FAB(umac_new_mf[mfi]),
+				                  BL_TO_FORTRAN_FAB(utemp_mf[mfi]));
+
+			}
+		}
+
+	}
 
 }
---
+++ Exec/UNIT_TESTS/test_projection/MaestroInit.cpp
@@ -0,0 +1,109 @@
+
+#include <Maestro.H>
+#include <AMReX_VisMF.H>
+#include <Problem_F.H>
+using namespace amrex;
+
+
+// initialize AMR data
+// perform initial projection
+// perform divu iters
+// perform initial (pressure) iterations
+void
+Maestro::Init ()
+{
+	// timer for profiling
+	BL_PROFILE_VAR("Maestro::Init()",Init);
+
+	Print() << "Calling Init()" << std::endl;
+
+	start_step = 1;
+
+	// fill in multifab and base state data
+	InitData();
+
+	// set finest_radial_level in fortran
+	// compute numdisjointchunks, r_start_coord, r_end_coord
+	init_multilevel(tag_array.dataPtr(),&finest_level);
+
+}
+
+// fill in multifab and base state data
+void
+Maestro::InitData ()
+{
+	// timer for profiling
+	BL_PROFILE_VAR("Maestro::InitData()",InitData);
+
+	Print() << "Calling InitData()" << std::endl;
+
+
+	// init_base_state(s0_init.dataPtr(),p0_init.dataPtr(),rho0_old.dataPtr(),
+	//                 rhoh0_old.dataPtr(),p0_old.dataPtr(),tempbar.dataPtr(),
+	//                 tempbar_init.dataPtr());
+
+	// calls AmrCore::InitFromScratch(), which calls a MakeNewGrids() function
+	// that repeatedly calls Maestro::MakeNewLevelFromScratch() to build and initialize
+	InitFromScratch(t_old);
+
+	// reset tagging array to include buffer zones
+	TagArray();
+
+	// set finest_radial_level in fortran
+	// compute numdisjointchunks, r_start_coord, r_end_coord
+	init_multilevel(tag_array.dataPtr(),&finest_level);
+
+	AverageDown(uold,0,AMREX_SPACEDIM);
+	FillPatch(t_old,uold,uold,uold,0,0,AMREX_SPACEDIM,0,bcs_u);
+
+}
+
+// During initialization of a simulation, Maestro::InitData() calls
+// AmrCore::InitFromScratch(), which calls
+// a MakeNewGrids() function that repeatedly calls this function to build
+// and initialize finer levels.  This function creates a new fine
+// level that did not exist before by interpolating from the coarser level
+// overrides the pure virtual function in AmrCore
+void Maestro::MakeNewLevelFromScratch (int lev, Real time, const BoxArray& ba,
+                                       const DistributionMapping& dm)
+{
+	// timer for profiling
+	BL_PROFILE_VAR("Maestro::MakeNewLevelFromScratch()",MakeNewLevelFromScratch);
+
+    sold              [lev].define(ba, dm,          Nscal, ng_s);
+    snew              [lev].define(ba, dm,          Nscal, ng_s);
+	uold              [lev].define(ba, dm, AMREX_SPACEDIM, ng_s);
+	unew              [lev].define(ba, dm, AMREX_SPACEDIM, ng_s);
+	gpi               [lev].define(ba, dm, AMREX_SPACEDIM,    1);
+    rhcc_for_nodalproj[lev].define(ba, dm,              1,    1);
+
+    pi[lev].define(convert(ba,nodal_flag), dm, 1, 1);     // nodal
+
+    sold              [lev].setVal(0.);
+    snew              [lev].setVal(0.);
+	uold              [lev].setVal(0.);
+	unew              [lev].setVal(0.);
+	gpi               [lev].setVal(0.);
+    rhcc_for_nodalproj[lev].setVal(0.);
+    pi                [lev].setVal(0.);
+
+	const Real* dx = geom[lev].CellSize();
+	const Real* dx_fine = geom[max_level].CellSize();
+
+	MultiFab& vel = uold[lev];
+
+	// Loop over boxes (make sure mfi takes a cell-centered multifab as an argument)
+#ifdef _OPENMP
+#pragma omp parallel
+#endif
+	for (MFIter mfi(scal, true); mfi.isValid(); ++mfi)
+	{
+		const Box& tilebox = mfi.tilebox();
+		const int* lo  = tilebox.loVect();
+		const int* hi  = tilebox.hiVect();
+
+		init_vel(ARLIM_3D(lo), ARLIM_3D(hi),
+		         BL_TO_FORTRAN_FAB(vel[mfi]),
+		         ZFILL(dx));
+	}
+}
