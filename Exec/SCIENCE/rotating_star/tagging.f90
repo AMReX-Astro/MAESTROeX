f53f06afbe274ca4d5465fb59745040c6676990e
Alice Harpole <aliceharpole@gmail.com>
2019-02-04 14:08:43 -0500

    fixed tagging.f90

--- Exec/SCIENCE/rotating_star/tagging.f90
+++ Exec/SCIENCE/rotating_star/tagging.f90
@@ -1,7 +1,9 @@
 module tagging_module
 
+  use parallel, only: parallel_IOProcessor
   use meth_params_module, only: temp_comp, rho_comp, nscal
   use probin_module, only: tag_density_3
+  use base_state_geometry_module, only: nr_fine, max_radial_level
 
   implicit none
 
@@ -64,5 +66,113 @@ contains
     enddo
 
   end subroutine state_error
+
+  subroutine tag_boxes(tag,tag_lo,tag_hi, &
+                        set,clear,&
+                        lo,hi,&
+                        dx,time,&
+                        lev,tag_array) bind(C, name="tag_boxes")
+
+    integer          :: lo(3),hi(3)
+    integer          :: tag_lo(3),tag_hi(3)
+    integer          :: tag(tag_lo(1):tag_hi(1),tag_lo(2):tag_hi(2),tag_lo(3):tag_hi(3))
+    double precision :: dx(3),time
+    integer          :: tag_array(0:max_radial_level,0:nr_fine-1)
+    integer          :: set,clear,lev
+
+    ! local
+    integer          :: i, j, k, r
+
+    if (parallel_IOProcessor()) then
+       print*,"Create a local copy of tagging.F90 in your build directory"
+       print*,"Here is a sample that tags the full grid using a predetermined tag array"
+    end if
+
+    ! abort program
+    call bl_error()
+
+
+    ! Tag on regions of high temperature
+#if (AMREX_SPACEDIM == 3)
+    do k = lo(3), hi(3)
+
+       if (tag_array(lev,k) > 0) then
+          do j = lo(2), hi(2)
+             do i = lo(1), hi(1)
+                tag(i,j,k) = set
+             enddo
+          enddo
+       endif
+
+    enddo
+
+#elif (AMREX_SPACEDIM == 2)
+    do k = lo(3), hi(3)
+       do j = lo(2), hi(2)
+
+          if (tag_array(lev,j) > 0) then
+             do i = lo(1), hi(1)
+                tag(i,j,k) = set
+             enddo
+          endif
+
+       enddo
+    enddo
+
+#else
+    do k = lo(3), hi(3)
+       do j = lo(2), hi(2)
+          do i = lo(1), hi(1)
+
+             if (tag_array(lev,i) > 0) then
+                tag(i,j,k) = set
+             endif
+
+          enddo
+       enddo
+    enddo
+
+#endif
+
+  end subroutine tag_boxes
+
+  subroutine retag_array(set,clear,&
+                          lo,hi,&
+                          lev,tag_array) bind(C, name="retag_array")
+
+    integer          :: lo(3),hi(3)
+    integer          :: tag_array(0:max_radial_level,0:nr_fine-1)
+    integer          :: set,clear,lev
+
+    ! local
+    integer          :: i, j, k, r
+
+
+    if (parallel_IOProcessor()) then
+       print*,"Create a local copy of tagging.F90 in your build directory"
+       print*,"Here is a sample that tags the refined region including buffer zone"
+    end if
+
+    ! abort program
+    call bl_error()
+
+    ! Tag on regions including buffer cells
+    do k = lo(3), hi(3)
+    do j = lo(2), hi(2)
+    do i = lo(1), hi(1)
+
+#if (AMREX_SPACEDIM == 3)
+          r = k/2
+#elif (AMREX_SPACEDIM == 2)
+          r = j/2
+#else
+          r = i/2
+#endif
+          tag_array(lev-1,r) = set
+    enddo
+    enddo
+    enddo
+
+  end subroutine retag_array
 
 end module tagging_module
