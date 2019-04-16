21de04de27f29071f6c164744b19c42f99f6615a
Donald E. Willcox <dewillcox@lbl.gov>
2019-04-16 11:17:53 -0700

    Make plot_deltat, max timestep, and max time plotfile conditions independent of plot_int > 0 condition for plotfiles and small plotfiles.

--- Source/MaestroEvolve.cpp
+++ Source/MaestroEvolve.cpp
@@ -102,19 +102,19 @@ Maestro::Evolve ()
                 Print() << "Time to advance time step: " << end_total << '\n';
 
 		// write a plotfile
-		if (plot_int > 0 && ( (istep % plot_int == 0) ||
-		                      (plot_deltat > 0 && std::fmod(t_new, plot_deltat) < dt) ||
-		                      (istep == max_step) ) || (t_old >= stop_time) )
+        if ( (plot_int > 0 && istep % plot_int == 0) ||
+             (plot_deltat > 0 && std::fmod(t_new, plot_deltat) < dt) ||
+             (istep == max_step) || (t_old >= stop_time) )
 		{
 			Print() << "\nWriting plotfile " << istep << std::endl;
 			WritePlotFile(istep,t_new,dt,rho0_new,rhoh0_new,p0_new,
 			              gamma1bar_new,unew,snew,S_cc_new);
 		}
 
-        if (small_plot_int > 0 && ( (istep % small_plot_int == 0) ||
-		                                   (small_plot_deltat > 0 &&
-		                                    std::fmod(t_new, small_plot_deltat) < dt) ||
-		                                   (istep == max_step)  || (t_old >= stop_time)) )
+        // write a small plotfile
+        if ( (small_plot_int > 0 && istep % small_plot_int == 0) ||
+             (small_plot_deltat > 0 && std::fmod(t_new, small_plot_deltat) < dt) ||
+             (istep == max_step)  || (t_old >= stop_time) )
 		{
 			Print() << "\nWriting small plotfile " << istep << std::endl;
 			WriteSmallPlotFile(istep,t_new,dt,rho0_new,rhoh0_new,p0_new,
