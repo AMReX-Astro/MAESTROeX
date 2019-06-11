6cabebcdaf5eccd35d07e6923c3057b3add942c3
Alice Harpole <aliceharpole@gmail.com>
2019-06-11 17:23:35 -0400

    Fixed test_stability so that it runs

--- Exec/TEST_PROBLEMS/test_stability/initdata.f90
+++ Exec/TEST_PROBLEMS/test_stability/initdata.f90
@@ -18,10 +18,10 @@ module initdata_module
 contains
 
   subroutine initdata(lev, time, lo, hi, &
-                      scal, scal_lo, scal_hi, nc_s, &
-                      vel, vel_lo, vel_hi, nc_v, &
-                      s0_init, p0_init, &
-                      dx) bind(C, name="initdata")
+       scal, scal_lo, scal_hi, nc_s, &
+       vel, vel_lo, vel_hi, nc_v, &
+       s0_init, p0_init, &
+       dx) bind(C, name="initdata")
 
     integer         , intent(in   ) :: lev, lo(3), hi(3)
     integer         , intent(in   ) :: scal_lo(3), scal_hi(3), nc_s
@@ -37,24 +37,24 @@ contains
     double precision, intent(in   ) :: p0_init(0:max_radial_level,0:nr_fine-1)
     double precision, intent(in   ) :: dx(3)
 
-    integer          :: i,j,k,r
+    integer          :: i,j,k,r,iloc,jloc,kloc
     integer :: ip, jp, kp
     double precision :: xloc(3), upert(3)
 
     double precision :: rand
 
     ! random numbers between -1 and 1
-    double precision :: alpha(3,3,3), beta(3,3,3), gamma(3,3,3)
+    double precision :: alpha(3,3), beta(3,3), gamma(3,3)
 
     ! random numbers between 0 and 2*pi
-    double precision :: phix(3,3,3), phiy(3,3,3), phiz(3,3,3)
+    double precision :: phix(3,3), phiy(3,3)
 
     ! L2 norm of k
-    double precision :: normk(3,3,3)
+    double precision :: norm(3,3)
 
     ! cos and sin of (2*pi*kx/L + phix), etc
-    double precision :: cx(3,3,3), cy(3,3,3), cz(3,3,3)
-    double precision :: sx(3,3,3), sy(3,3,3), sz(3,3,3)
+    double precision :: cx(3,3), cy(3,3)
+    double precision :: sx(3,3), sy(3,3)
 
     double precision :: rloc
 
@@ -64,103 +64,92 @@ contains
     ! generate random numbers
     ! random numbers are not currently supported
     ! use functions that result in numbers between (-1,1)
-    do k=1,3
-       do j=1,3
-          do i=1,3
-             rand = amrex_random()
-             rand = 2.0d0*rand - 1.0d0
-             ! rand = (.5)**i * (.7)**j * (.3)**k * (-1.)**i
-             alpha(i,j,k) = rand
-             rand = amrex_random()
-             rand = 2.0d0*rand - 1.0d0
-             ! rand = (.5)**i * (.3)**j * (.7)**k * (-1.)**j
-             beta(i,j,k) = rand
-             rand = amrex_random()
-             rand = 2.0d0*rand - 1.0d0
-             ! rand = (.3)**i * (.5)**j * (.7)**k * (-1.)**k
-             gamma(i,j,k) = rand
-             rand = amrex_random()
-             ! rand = (.3)**i * (.7)**j * (.5)**k
-             rand = 2.0d0*M_PI*rand
-             phix(i,j,k) = rand
-             rand = amrex_random()
-             ! rand = (.7)**i * (.3)**j * (.5)**k
-             rand = 2.0d0*M_PI*rand
-             phiy(i,j,k) = rand
-             rand = amrex_random()
-             ! rand = (.7)**i * (.5)**j * (.3)**k
-             rand = 2.0d0*M_PI*rand
-             phiz(i,j,k) = rand
-          enddo
-       enddo
-    enddo
-
-    ! compute the norm of k
-    do k=1,3
-       do j=1,3
-          do i=1,3
-             normk(i,j,k) = sqrt(dble(i)**2+dble(j)**2+dble(k)**2)
-          enddo
-       enddo
-    enddo
-
-    do k=lo(3), hi(3)
-       xloc(3) = prob_lo(3) + (dble(k)+0.5d0)*dx(3)
-
-       do j=lo(2),hi(2)
-          xloc(2) = prob_lo(2) + (dble(j)+0.5d0)*dx(2)
-
-          do i=lo(1),hi(1)
-             xloc(1) = prob_lo(1) + (dble(i)+0.5d0)*dx(1)
-
-             upert = 0.d0
-
-             rloc = xloc(2)
-
-             ! loop over the 9 combinations of fourier components
-             do kp = 1, 3
-                do jp = 1, 3
-                   do ip = 1, 3
-                      ! compute cosines and sines
-                      cx(ip,jp,kp) = cos(2.0d0*M_PI*dble(i)*xloc(1)/velpert_scale + phix(ip,jp,kp))
-                      cy(ip,jp,kp) = cos(2.0d0*M_PI*dble(j)*xloc(2)/velpert_scale + phiy(ip,jp,kp))
-                      sx(ip,jp,kp) = sin(2.0d0*M_PI*dble(i)*xloc(1)/velpert_scale + phix(ip,jp,kp))
-                      sy(ip,jp,kp) = sin(2.0d0*M_PI*dble(j)*xloc(2)/velpert_scale + phiy(ip,jp,kp))
-                   end do
-                end do
-             end do
-
-             ! loop over the 9 combinations of fourier components
-             do kp = 1, 3
-                do jp = 1, 3
-                   do ip = 1, 3
-                      ! compute contribution from perturbation velocity from each mode
-                      upert(1) = upert(1) + &
-                           (-gamma(ip,jp,kp)*dble(j)*cx(ip,jp,kp)*sy(ip,jp,kp) &
-                             +beta(ip,jp,kp)*dble(j)*cy(ip,jp,kp)*sx(ip,jp,kp)) &
-                             / normk(ip,jp,kp)
-
-                      upert(2) = upert(2) + &
-                           (gamma(ip,jp,kp)*dble(i)*cy(ip,jp,kp)*sx(ip,jp,kp) &
-                           -alpha(ip,jp,kp)*dble(j)*cx(ip,jp,kp)*sy(ip,jp,kp)) &
-                             / normk(ip,jp,kp)
-
-                   end do
-                end do
-             end do
-
-             ! apply the cutoff function to the perturbational velocity
-             do ip = 1, nc_v
-                upert(ip) = velpert_amplitude*upert(ip) &
-                     *(0.5d0+0.5d0*tanh((velpert_radius-rloc)/velpert_steep))
-             end do
-
-             ! add perturbational velocity to background velocity
-             vel(i,j,k,1:nc_v) = vel(i,j,k,1:nc_v) + upert(1:nc_v)
-
-          end do
-       end do
-    end do
+    ! 
+    ! do j=1,3
+    !    do i=1,3
+    !       rand = amrex_random()
+    !       rand = 2.0d0*rand - 1.0d0
+    !       ! rand = (.5)**i * (.7)**j * (.3)**k * (-1.)**i
+    !       alpha(i,j) = rand
+    !       rand = amrex_random()
+    !       rand = 2.0d0*rand - 1.0d0
+    !       ! rand = (.5)**i * (.3)**j * (.7)**k * (-1.)**j
+    !       beta(i,j) = rand
+    !       rand = amrex_random()
+    !       rand = 2.0d0*rand - 1.0d0
+    !       ! rand = (.3)**i * (.5)**j * (.7)**k * (-1.)**k
+    !       gamma(i,j) = rand
+    !       rand = amrex_random()
+    !       ! rand = (.3)**i * (.7)**j * (.5)**k
+    !       rand = 2.0d0*M_PI*rand
+    !       phix(i,j) = rand
+    !       rand = amrex_random()
+    !       ! rand = (.7)**i * (.3)**j * (.5)**k
+    !       rand = 2.0d0*M_PI*rand
+    !       phiy(i,j) = rand
+    !       rand = amrex_random()
+    !    enddo
+    ! enddo
+    !
+    ! ! compute the norm s
+    !
+    ! do j=1,3
+    !    do i=1,3
+    !       norm(i,j) = sqrt(dble(i)**2+dble(j)**2)
+    !    enddo
+    ! enddo
+    !
+    ! do kloc=lo(3), hi(3)
+    !    do jloc=lo(2),hi(2)
+    !       xloc(2) = prob_lo(2) + (dble(jloc)+0.5d0)*dx(2)
+    !
+    !       do iloc=lo(1),hi(1)
+    !          xloc(1) = prob_lo(1) + (dble(iloc)+0.5d0)*dx(1)
+    !
+    !          upert = 0.d0
+    !
+    !          rloc = xloc(2)
+    !
+    !          ! loop over the 9 combinations of fourier components
+    !          do i=1,3
+    !             do j=1,3
+    !                ! compute cosines and sines
+    !                cx(i,j) = cos(2.0d0*M_PI*dble(i)*xloc(1)/velpert_scale + phix(i,j))
+    !                cy(i,j) = cos(2.0d0*M_PI*dble(j)*xloc(2)/velpert_scale + phiy(i,j))
+    !                sx(i,j) = sin(2.0d0*M_PI*dble(i)*xloc(1)/velpert_scale + phix(i,j))
+    !                sy(i,j) = sin(2.0d0*M_PI*dble(j)*xloc(2)/velpert_scale + phiy(i,j))
+    !             enddo
+    !          enddo
+    !
+    !          ! loop over the 9 combinations of fourier components
+    !          do i=1,3
+    !             do j=1,3
+    !                ! compute contribution from perturbation velocity from each mode
+    !                upert(1) = upert(1) + &
+    !                     (-gamma(i,j)*dble(j)*cx(i,j)*sy(i,j) &
+    !                     +beta(i,j)*dble(j)*cy(i,j)*sx(i,j)) &
+    !                     / norm(i,j)
+    !
+    !                upert(2) = upert(2) + &
+    !                     (gamma(i,j)*dble(i)*cy(i,j)*sx(i,j) &
+    !                     -alpha(i,j)*dble(j)*cx(i,j)*sy(i,j)) &
+    !                     / norm(i,j)
+    !
+    !             enddo
+    !          enddo
+    !
+    !          ! apply the cutoff function to the perturbational velocity
+    !          do ip = 1, 2
+    !             upert(ip) = velpert_amplitude*upert(ip) &
+    !                  *(0.5d0+0.5d0*tanh((velpert_radius-rloc)/velpert_steep))
+    !          end do
+    !
+    !          ! add perturbational velocity to background velocity
+    !          vel(iloc,jloc,kloc,1:2) = vel(iloc,jloc,kloc,1:2) + upert(1:2)
+    !
+    !       end do
+    !    end do
+    ! enddo
 
     do k=lo(3),hi(3)
        do j=lo(2),hi(2)
