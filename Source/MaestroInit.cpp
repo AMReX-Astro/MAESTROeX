6016d421ee95a022fa17a70a824c82c70970aff9
Alice Harpole <aliceharpole@gmail.com>
2019-07-02 17:06:17 +0200

    moved diagnostics file printing outside of chk_int > 0 if statement in MaestroInit.cpp

--- Source/MaestroInit.cpp
+++ Source/MaestroInit.cpp
@@ -188,8 +188,10 @@ Maestro::Init ()
 		if (chk_int > 0) {
 			Print() << "\nWriting checkpoint 0 after all initialization" << std::endl;
 			WriteCheckPoint(0);
+        }
 
-			int index_dummy=0;
+        if (sum_interval > 0  || sum_per > 0) {
+			int index_dummy = 0;
 			Print() << "\nWriting diagnosis file after all initialization" << std::endl;
 			DiagFile(0,t_old,rho0_old,p0_old,uold,sold,index_dummy);
 		}
