fd1185875b21c75c2b214d204d91fc9c59a29aba
Doreen Fan <dfan@lbl.gov>
2019-08-19 11:54:42 -0700

    added MakeIntraCoeffs subroutine with gpu

--- Source/MaestroIntra.cpp
+++ Source/MaestroIntra.cpp
@@ -3,24 +3,31 @@
 
 using namespace amrex;
 
-// compute heating term, rho_Hext
+// compute cp and xi
 void
-Maestro::MakeIntraCoeffs (const Vector<MultiFab>& s1,
-			  const Vector<MultiFab>& s2,
+Maestro::MakeIntraCoeffs (const Vector<MultiFab>& scal1,
+			  const Vector<MultiFab>& scal2,
 			  Vector<MultiFab>& cp,
 			  Vector<MultiFab>& xi)
 {
     // timer for profiling
     BL_PROFILE_VAR("Maestro::MakeIntraCoeffs()",MakeIntraCoeffs);
 
+#ifdef AMREX_USE_CUDA
+    auto not_launched = Gpu::notInLaunchRegion();
+    // turn on GPU
+    if (not_launched) Gpu::setLaunchRegion(true);
+#endif
+
     for (int lev=0; lev<=finest_level; ++lev) {
 
         // get references to the MultiFabs at level lev
-	const MultiFab& s1_mf = s1[lev];
-	const MultiFab& s2_mf = s2[lev];
+	const MultiFab& s1_mf = scal1[lev];
+	const MultiFab& s2_mf = scal2[lev];
               MultiFab& cp_mf = cp[lev];
               MultiFab& xi_mf = xi[lev];
 
+        Print() << "... Level " << lev << " create intra coeffs:" << std::endl;
 
         // loop over boxes (make sure mfi takes a cell-centered multifab as an argument)
 #ifdef _OPENMP
@@ -29,18 +36,21 @@ Maestro::MakeIntraCoeffs (const Vector<MultiFab>& s1,
         for ( MFIter mfi(s1_mf, true); mfi.isValid(); ++mfi ) {
 
             // Get the index space of the valid region
-            const Box& tileBox = mfi.tilebox();
+            const Box& gtbx = mfi.growntilebox(1);
             const Real* dx = geom[lev].CellSize();
-
-            // call fortran subroutine
 	    
-            // make_intra_coeffs(ARLIM_3D(tileBox.loVect()), ARLIM_3D(tileBox.hiVect()),
-	    // 		      BL_TO_FORTRAN_3D(rho_Hext_mf[mfi]),
-	    // 		      BL_TO_FORTRAN_FAB(scal_mf[mfi]), dx, &t_old);
+            // call fortran subroutine
+#pragma gpu box(gtbx)
+            make_intra_coeffs(AMREX_INT_ANYD(gtbx.loVect()), AMREX_INT_ANYD(gtbx.hiVect()),
+	     		      BL_TO_FORTRAN_ANYD(s1_mf[mfi]),
+	     		      BL_TO_FORTRAN_ANYD(s2_mf[mfi]),
+			      BL_TO_FORTRAN_ANYD(cp_mf[mfi]),
+			      BL_TO_FORTRAN_ANYD(xi_mf[mfi]));
         }
     }
 
-    // average down
-    AverageDown(cp,0,1);
-    AverageDown(xi,0,1);
+#ifdef AMREX_USE_CUDA
+    // turn off GPU
+    if (not_launched) Gpu::setLaunchRegion(false);
+#endif
 }
---
+++ Source/Src_nd/make_intra_coeffs.F90
@@ -0,0 +1,81 @@
+! create the coefficients needed for I_T (intra for the SDC
+! predict_T_* enthalpy updates).  We take an old and new state
+! and return the time-centered quantities.
+
+
+module make_intra_coeffs_module
+
+  use meth_params_module, only: rho_comp, temp_comp, spec_comp, nscal
+  use eos_module
+  use eos_type_module
+  use network, only: nspec
+  use amrex_constants_module
+
+  implicit none
+
+  private
+
+contains 
+  
+  subroutine make_intra_coeffs(lo, hi, &
+       scalold, s1_lo, s1_hi, &
+       scalnew, s2_lo, s2_hi, &
+       cp, cp_lo, cp_hi, &
+       xi, xi_lo, xi_hi) bind(C,name="make_intra_coeffs")
+    ! input box is grown to include boundary cell information
+    
+    integer         , intent(in   ) :: lo(3), hi(3)
+    integer         , intent(in   ) :: s1_lo(3), s1_hi(3)
+    double precision, intent(in   ) :: scalold(s1_lo(1):s1_hi(1),s1_lo(2):s1_hi(2),s1_lo(3):s1_hi(3),nscal)
+    integer         , intent(in   ) :: s2_lo(3), s2_hi(3)
+    double precision, intent(in   ) :: scalnew(s2_lo(1):s2_hi(1),s2_lo(2):s2_hi(2),s2_lo(3):s2_hi(3),nscal)
+    integer         , intent(in   ) :: cp_lo(3), cp_hi(3)
+    double precision, intent(inout) :: cp(cp_lo(1):cp_hi(1),cp_lo(2):cp_hi(2),cp_lo(3):cp_hi(3))
+    integer         , intent(in   ) :: xi_lo(3), xi_hi(3)
+    double precision, intent(inout) :: xi(xi_lo(1):xi_hi(1),xi_lo(2):xi_hi(2),xi_lo(3):xi_hi(3),nspec)
+
+    ! local
+    integer :: i,j,k,comp
+    type (eos_t) :: eos_state
+
+    !$gpu
+    
+    do k=lo(3),hi(3)
+       do j=lo(2),hi(2)
+          do i=lo(1),hi(1)
+             
+             ! old state first
+             eos_state%rho   = scalold(i,j,k,rho_comp)
+             eos_state%T     = scalold(i,j,k,temp_comp)
+             eos_state%xn(:) = scalold(i,j,k,spec_comp:spec_comp+nspec-1)/eos_state%rho
+             
+             ! dens, temp, and xmass are inputs
+             call eos(eos_input_rt, eos_state)
+
+             cp(i,j,k) = eos_state%cp
+             
+             do comp=1,nspec
+                xi(i,j,k,comp) = eos_state%dhdX(comp)
+             enddo
+
+             ! new state now -- average results
+             eos_state%rho   = scalnew(i,j,k,rho_comp)
+             eos_state%T     = scalnew(i,j,k,temp_comp)
+             eos_state%xn(:) = scalnew(i,j,k,spec_comp:spec_comp+nspec-1)/eos_state%rho
+             
+             ! dens, temp, and xmass are inputs
+             call eos(eos_input_rt, eos_state)
+             
+             cp(i,j,k) = HALF*(eos_state%cp + cp(i,j,k))
+             
+             do comp=1,nspec
+                xi(i,j,k,comp) = HALF*(eos_state%dhdX(comp) + xi(i,j,k,comp))
+             enddo
+
+          enddo
+       enddo
+    enddo
+    
+  end subroutine make_intra_coeffs
+
+end module make_intra_coeffs_module
