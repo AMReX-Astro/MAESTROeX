43fb7079f821ecb4edc8c0544bfabb0672dd1330
Doreen Fan <dfan@lbl.gov>
2019-08-21 14:21:10 -0700

    add instantaneous_reaction_rates subroutine for sdc

--- Source/MaestroReact.cpp
+++ Source/MaestroReact.cpp
@@ -353,3 +353,44 @@ void Maestro::Burner(const Vector<MultiFab>& s_in,
     }
 }
 #endif
+
+
+// compute heating terms, rho_omegadot and rho_Hnuc
+void
+Maestro::MakeReactionRates (Vector<MultiFab>& rho_omegadot,
+			    Vector<MultiFab>& rho_Hnuc,
+			    const Vector<MultiFab>& scal)
+{
+    // timer for profiling
+    BL_PROFILE_VAR("Maestro::MakeReactionRates()",MakeReactionRates);
+
+    for (int lev=0; lev<=finest_level; ++lev) {
+
+        // get references to the MultiFabs at level lev
+              MultiFab& rho_omegadot_mf = rho_omegadot[lev];
+              MultiFab&     rho_Hnuc_mf = rho_Hnuc[lev];
+        const MultiFab&         scal_mf =     scal[lev];
+
+
+        // loop over boxes (make sure mfi takes a cell-centered multifab as an argument)
+#ifdef _OPENMP
+#pragma omp parallel
+#endif
+        for ( MFIter mfi(scal_mf, true); mfi.isValid(); ++mfi ) {
+
+            // Get the index space of the valid region
+            const Box& tileBox = mfi.tilebox();
+            const Real* dx = geom[lev].CellSize();
+
+            // call fortran subroutine
+            // use macros in AMReX_ArrayLim.H to pass in each FAB's data,
+            // lo/hi coordinates (including ghost cells), and/or the # of components
+            // We will also pass "validBox", which specifies the "valid" region.
+            instantaneous_reaction_rates(ARLIM_3D(tileBox.loVect()), ARLIM_3D(tileBox.hiVect()),
+					 BL_TO_FORTRAN_3D(rho_omegadot_mf[mfi]),
+					 BL_TO_FORTRAN_3D(rho_Hnuc_mf[mfi]),
+					 BL_TO_FORTRAN_3D(scal_mf[mfi]));
+        }
+    }
+    
+}
