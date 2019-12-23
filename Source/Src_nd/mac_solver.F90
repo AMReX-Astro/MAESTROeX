35f9f361d25b3ae4c994b0122e1e28a27fc22770
Alice Harpole <aliceharpole@gmail.com>
2019-12-23 14:38:37 -0500

    offload mult_beta0

--- Source/Src_nd/mac_solver.F90
+++ Source/Src_nd/mac_solver.F90
@@ -58,135 +58,76 @@ contains
 
   end subroutine mac_solver_rhs
 
-  subroutine mult_beta0(lev, lo, hi, &
+  subroutine mult_beta0(lo, hi, lev, idir,  &
        uedge, u_lo, u_hi, &
-       vedge, v_lo, v_hi, &
-#if (AMREX_SPACEDIM == 3)
-       wedge, w_lo, w_hi, &
-#endif
        beta0_edge, &
        beta0, &
        mult_or_div) bind(C, name="mult_beta0")
 
-    integer         , intent(in   ) :: lev, lo(3), hi(3)
+    integer         , intent(in   ) :: lo(3), hi(3)
+    integer  , value, intent(in   ) :: lev, idir
     integer         , intent(in   ) :: u_lo(3), u_hi(3)
     double precision, intent(inout) :: uedge(u_lo(1):u_hi(1),u_lo(2):u_hi(2),u_lo(3):u_hi(3))
-    integer         , intent(in   ) :: v_lo(3), v_hi(3)
-    double precision, intent(inout) :: vedge(v_lo(1):v_hi(1),v_lo(2):v_hi(2),v_lo(3):v_hi(3))
-#if (AMREX_SPACEDIM == 3)
-    integer         , intent(in   ) :: w_lo(3), w_hi(3)
-    double precision, intent(inout) :: wedge(w_lo(1):w_hi(1),w_lo(2):w_hi(2),w_lo(3):w_hi(3))
-#endif
     double precision, intent(in   ) :: beta0_edge(0:max_radial_level,0:nr_fine)
     double precision, intent(in   ) :: beta0(0:max_radial_level,0:nr_fine-1)
-    integer, intent(in   ) :: mult_or_div
+    integer  , value, intent(in   ) :: mult_or_div
 
     ! local
     integer i,j,k
 
     ! Neglecting ghost cells
     if (mult_or_div .eq. 1) then
+        ! Multiply
 
-       ! Multiply
-#if (AMREX_SPACEDIM == 2)
-       k = lo(3)
-       ! Use cell-centered beta0 to update u-velocity
-       do j = lo(2),hi(2)
-          do i = lo(1),hi(1)+1
-             uedge(i,j,k) = uedge(i,j,k) * beta0(lev,j)
-          end do
-       end do
-       ! Use edge beta0 to update v-velocity
-       do j = lo(2),hi(2)+1
-          do i = lo(1),hi(1)
-             vedge(i,j,k) = vedge(i,j,k) * beta0_edge(lev,j)
-          end do
-       end do
+        if (idir /= AMREX_SPACEDIM) then
+            ! Use cell-centered beta0 to update horizontal velocity
+            do k = lo(3),hi(3)
+                do j = lo(2),hi(2)
+                    do i = lo(1),hi(1)
+                        uedge(i,j,k) = uedge(i,j,k) * beta0(lev,k)
+                    end do
+                end do
+            end do
 
-#elif (AMREX_SPACEDIM == 3)
-       ! Use cell-centered beta0 to update u-velocity
-       !$OMP PARALLEL DO PRIVATE(i,j,k)
-       do k = lo(3),hi(3)
-          do j = lo(2),hi(2)
-             do i = lo(1),hi(1)+1
-                uedge(i,j,k) = uedge(i,j,k) * beta0(lev,k)
-             end do
-          end do
-       end do
-       !$OMP END PARALLEL DO
-       ! Use edge beta0 to update v-velocity
-       !$OMP PARALLEL DO PRIVATE(i,j,k)
-       do k = lo(3),hi(3)
-          do j = lo(2),hi(2)+1
-             do i = lo(1),hi(1)
-                vedge(i,j,k) = vedge(i,j,k) * beta0(lev,k)
-             end do
-          end do
-       end do
-       !$OMP END PARALLEL DO
-       ! Use cell-centered beta0 to update w-velocity
-       !$OMP PARALLEL DO PRIVATE(i,j,k)
-       do k = lo(3),hi(3)+1
-          do j = lo(2),hi(2)
-             do i = lo(1),hi(1)
-                wedge(i,j,k) = wedge(i,j,k) * beta0_edge(lev,k)
-             end do
-          end do
-       end do
-       !$OMP END PARALLEL DO
-#endif
+        else
+
+            ! Use cell-centered beta0 to update vertical velocity
+            do k = lo(3),hi(3)
+                do j = lo(2),hi(2)
+                    do i = lo(1),hi(1)
+                        uedge(i,j,k) = uedge(i,j,k) * beta0_edge(lev,k)
+                    end do
+                end do
+            end do
+        
+        endif
 
     else if (mult_or_div .eq. 0) then
-
        ! Divide
-#if (AMREX_SPACEDIM == 2)
-       k = lo(3)
-       ! Use cell-centered beta0 to update u-velocity
-       do j = lo(2),hi(2)
-          do i = lo(1),hi(1)+1
-             uedge(i,j,k) = uedge(i,j,k) / beta0(lev,j)
-          end do
-       end do
-       ! Use edge beta0 to update v-velocity
-       do j = lo(2),hi(2)+1
-          do i = lo(1),hi(1)
-             vedge(i,j,k) = vedge(i,j,k) / beta0_edge(lev,j)
-          end do
-       end do
 
-#elif (AMREX_SPACEDIM == 3)
-       ! Use cell-centered beta0 to update u-velocity
-       !$OMP PARALLEL DO PRIVATE(i,j,k)
-       do k = lo(3),hi(3)
-          do j = lo(2),hi(2)
-             do i = lo(1),hi(1)+1
-                uedge(i,j,k) = uedge(i,j,k) / beta0(lev,k)
-             end do
-          end do
-       end do
-       !$OMP END PARALLEL DO
-       ! Use edge beta0 to update v-velocity
-       !$OMP PARALLEL DO PRIVATE(i,j,k)
-       do k = lo(3),hi(3)
-          do j = lo(2),hi(2)+1
-             do i = lo(1),hi(1)
-                vedge(i,j,k) = vedge(i,j,k) / beta0(lev,k)
-             end do
-          end do
-       end do
-       !$OMP END PARALLEL DO
-       ! Use cell-centered beta0 to update w-velocity
-       !$OMP PARALLEL DO PRIVATE(i,j,k)
-       do k = lo(3),hi(3)+1
-          do j = lo(2),hi(2)
-             do i = lo(1),hi(1)
-                wedge(i,j,k) = wedge(i,j,k) / beta0_edge(lev,k)
-             end do
-          end do
-       end do
-       !$OMP END PARALLEL DO
-#endif
+        if (idir /= AMREX_SPACEDIM) then
 
+            ! Use cell-centered beta0 to update horizontal velocity
+            do k = lo(3),hi(3)
+                do j = lo(2),hi(2)
+                    do i = lo(1),hi(1)
+                        uedge(i,j,k) = uedge(i,j,k) / beta0(lev,k)
+                    end do
+                end do
+            end do
+
+        else 
+
+            ! Use cell-centered beta0 to update vertical velocity
+            do k = lo(3),hi(3)
+                do j = lo(2),hi(2)
+                    do i = lo(1),hi(1)
+                        uedge(i,j,k) = uedge(i,j,k) / beta0_edge(lev,k)
+                    end do
+                end do
+            end do
+
+        end if
     end if
 
   end subroutine mult_beta0
