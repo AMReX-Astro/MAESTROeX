c73c783dcc5a9b81c43ed6c5906f735f86947859
Alice Harpole <aliceharpole@gmail.com>
2020-02-11 14:37:42 -0500

    fixed rsa keys

--- Util/scripts/pr_tab_remover.sh
+++ Util/scripts/pr_tab_remover.sh
@@ -6,7 +6,7 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 
 # Add rsa keys to the ssh agent to push to GitHub
-gpg --output ../id_maestro_rsa --batch --passphrase $DECRYPT_GITHUB_AUTH --decrypt id_maestro_rsa.enc
+gpg --output ../id_maestro_rsa --batch --passphrase $DECRYPT_GITHUB_AUTH --decrypt $TRAVIS_BUILD_DIR/id_maestro_rsa.enc
 chmod 600 ../id_maestro_rsa
 eval `ssh-agent -s`
 ssh-add ../id_maestro_rsa
@@ -26,16 +26,9 @@ echo "doing git add/commit/push"
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
-git add --all
-
-# Exit if there are no docs changes
-if git diff --staged --quiet; then
-   echo "exiting with no changes"
-   exit 0
-fi
 
 # Otherwise, commit and push
-git commit -m "Tabs have been converted to spaces by tab_exterminator.sh"
+git commit -am "Tabs have been converted to spaces by tab_exterminator.sh"
 git push $SSH_REPO $TRAVIS_PULL_REQUEST_BRANCH
 cd 
 
