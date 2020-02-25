1b826c6fc9d92e5a2d84c19e8dfdf7b4d31237da
Alice Harpole <aliceharpole@gmail.com>
2020-02-25 10:21:36 -0500

    GPU fix

--- Source/MaestroFillData.cpp
+++ Source/MaestroFillData.cpp
@@ -205,7 +205,7 @@ Maestro::AverageDownFaces (Vector<std::array< MultiFab, AMREX_SPACEDIM > >& edge
 // fill in ONE ghost cell for all components of a face-centered (MAC) velocity
 // field behind physical boundaries.  Does not modify the velocities on the boundary
 void
-Maestro::FillUmacGhost (Vector<std::array< MultiFab, AMREX_SPACEDIM > >& umac,
+Maestro::FillUmacGhost (Vector<std::array< MultiFab, AMREX_SPACEDIM > >& umac_in,
                         int level)
 {
     // timer for profiling
@@ -226,14 +226,18 @@ Maestro::FillUmacGhost (Vector<std::array< MultiFab, AMREX_SPACEDIM > >& umac,
 
         // Get the index space of the domain
         const Box& domainBox = geom[lev].Domain();
+        const auto domlo = domainBox.loVect3d();
+        const auto domhi = domainBox.hiVect3d();
+
+        const int * AMREX_RESTRICT physbc_p = phys_bc.dataPtr();
 
         // get references to the MultiFabs at level lev
         MultiFab& sold_mf = sold[lev];         // need a cell-centered MF for the MFIter
-        MultiFab& umacx_mf = umac[lev][0];
-        MultiFab& umacy_mf = umac[lev][1];
-#if (AMREX_SPACEDIM == 3)
-        MultiFab& umacz_mf = umac[lev][2];
-#endif
+//         MultiFab& umacx_mf = umac[lev][0];
+//         MultiFab& umacy_mf = umac[lev][1];
+// #if (AMREX_SPACEDIM == 3)
+//         MultiFab& umacz_mf = umac[lev][2];
+// #endif
 
         // DO NOT TILE THIS SUBROUTINE
         // this just filling ghost cells so the fortran logic has to be reworked
@@ -242,15 +246,248 @@ Maestro::FillUmacGhost (Vector<std::array< MultiFab, AMREX_SPACEDIM > >& umac,
 
             // Get the index space of the valid (cell-centered) region
             const Box& tilebox = mfi.tilebox();
+            const auto xbx = mfi.grownnodaltilebox(0, 1);
+            const auto ybx = mfi.grownnodaltilebox(1, 1);
+            const auto zbx = mfi.grownnodaltilebox(2, 1);
 
-            fill_umac_ghost(ARLIM_3D(domainBox.loVect()), ARLIM_3D(domainBox.hiVect()),
-                            ARLIM_3D(tilebox.loVect()), ARLIM_3D(tilebox.hiVect()),
-                            BL_TO_FORTRAN_3D(umacx_mf[mfi]),
-                            BL_TO_FORTRAN_3D(umacy_mf[mfi]),
+            const Array4<Real> umac = umac_in[lev][0].array(mfi);
+            const Array4<Real> vmac = umac_in[lev][1].array(mfi);
 #if (AMREX_SPACEDIM == 3)
-                            BL_TO_FORTRAN_3D(umacz_mf[mfi]),
+            const Array4<Real> wmac = umac_in[lev][2].array(mfi);
 #endif
-                            phys_bc.dataPtr());
+
+        AMREX_PARALLEL_FOR_3D(xbx, i, j, k, 
+        {
+            // lo x-faces
+            if (i == domlo[0] - 1) {
+                switch(physbc_p[0]) {
+                    case Inflow:
+                        umac(i,j,k) = umac(i+1,j,k);
+                        vmac(i,j,k) = 0.0;
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i,j,k) = 0.0;
+#endif
+                        break;
+                    case SlipWall:
+                    case NoSlipWall:
+                        umac(i,j,k) = 0.0;
+                        vmac(i,j,k) = 0.0;
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i,j,k) = 0.0;
+#endif
+                        break;
+                    case Outflow:
+                        umac(i,j,k) = umac(i+1,j,k);
+                        vmac(i,j,k) = vmac(i+1,j,k);
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i,j,k) = wmac(i+1,j,k);
+#endif
+                        break;
+                    case Symmetry:
+                        umac(i,j,k) = -umac(i+2,j,k);
+                        vmac(i,j,k) = vmac(i+1,j,k);
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i,j,k) = wmac(i+1,j,k);
+#endif
+                        break;
+                    case Interior:
+                    // do nothing
+                        break;
+                }
+            }
+
+            // hi x-faces
+            if (i == domhi[0]+2) {
+                switch(physbc_p[AMREX_SPACEDIM]) {
+                    case Inflow:
+                        umac(i,j,k) = umac(i-1,j,k);
+                        vmac(i-1,j,k) = 0.0;
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i-1,j,k) = 0.0;
+#endif
+                        break;
+                    case SlipWall:
+                    case NoSlipWall:
+                        umac(i,j,k) = 0.0;
+                        vmac(i-1,j,k) = 0.0;
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i-1,j,k) = 0.0;
+#endif
+                        break;
+                    case Outflow:
+                        umac(i,j,k) = umac(i-1,j,k);
+                        vmac(i-1,j,k) = vmac(i-2,j,k);
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i-1,j,k) = wmac(i-2,j,k);
+#endif
+                        break;
+                    case Symmetry:
+                        umac(i,j,k) = -umac(i-2,j,k);
+                        vmac(i-1,j,k) = vmac(i-2,j,k);
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i-1,j,k) = wmac(i-2,j,k);
+#endif
+                        break;
+                    case Interior:
+                        // do nothing
+                        break;        
+                }
+            }
+        });
+
+        AMREX_PARALLEL_FOR_3D(ybx, i, j, k, 
+        {
+            // lo y-faces
+            if (j == domlo[1]-1) {
+                switch (physbc_p[1]) {
+                    case Inflow:
+                        umac(i,j,k) = 0.0;
+                        vmac(i,j,k) = vmac(i,j+1,k);
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i,j,k) = 0.0;
+#endif
+                        break;
+                    case SlipWall:
+                    case NoSlipWall:
+                        umac(i,j,k) = 0.0;
+                        vmac(i,j,k) = 0.0;
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i,j,k) = 0.0;
+#endif
+                        break;
+                    case Outflow:
+                        umac(i,j,k) = umac(i,j+1,k);
+                        vmac(i,j,k) = vmac(i,j+1,k);
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i,j,k) = wmac(i,j+1,k);
+#endif
+                        break;
+                    case Symmetry:
+                        umac(i,j,k) = umac(i,j+1,k);
+                        vmac(i,j,k) = -vmac(i,j+2,k);
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i,j,k) = wmac(i,j+1,k);
+#endif
+                        break;
+                    case Interior:
+                        // do nothing
+                        break;            
+                }
+            }
+
+            // hi y-faces
+            if (j == domhi[1]+2) {
+                switch (physbc_p[AMREX_SPACEDIM+1]) {
+                    case Inflow:
+                        umac(i,j-1,k) = 0.0;
+                        vmac(i,j,k) = vmac(i,j-1,k);
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i,j-1,k) = 0.0;
+#endif
+                        break;
+                    case SlipWall:
+                    case NoSlipWall:
+                        umac(i,j-1,k) = 0.0;
+                        vmac(i,j,k) = 0.0;
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i,j-1,k) = 0.0;
+#endif
+                        break;
+                    case Outflow:
+                        umac(i,j-1,k) = umac(i,j-2,k);
+                        vmac(i,j,k) = vmac(i,j-1,k);
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i,j-1,k) = wmac(i,j-2,k);
+#endif
+                        break;
+                    case Symmetry:
+                        umac(i,j-1,k) = umac(i,j-2,k);
+                        vmac(i,j,k) = -vmac(i,j-2,k);
+#if (AMREX_SPACEDIM == 3)
+                        wmac(i,j-1,k) = wmac(i,j-2,k);
+#endif
+                        break;
+                    case Interior:
+                        // do nothing
+                        break;            
+                }
+            }
+        });
+
+#if (AMREX_SPACEDIM == 3)
+
+        AMREX_PARALLEL_FOR_3D(zbx, i, j, k, 
+        {
+            // lo z-faces
+            if (k == domlo[2]-1) {
+                switch (physbc_p[2]) {
+                    case Inflow:
+                        umac(i,j,k) = 0.0;
+                        vmac(i,j,k) = 0.0;
+                        wmac(i,j,k) = wmac(i,j,k+1);
+                        break;
+                    case SlipWall:
+                    case NoSlipWall:
+                        umac(i,j,k) = 0.0;
+                        vmac(i,j,k) = 0.0;
+                        wmac(i,j,k) = 0.0;
+                        break;
+                    case Outflow:
+                        umac(i,j,k) = umac(i,j,k+1);
+                        vmac(i,j,k) = vmac(i,j,k+1);
+                        wmac(i,j,k) = wmac(i,j,k+1);
+                        break;
+                    case Symmetry:
+                        umac(i,j,k) = umac(i,j,k+1);
+                        vmac(i,j,k) = vmac(i,j,k+1);
+                        wmac(i,j,k) = -wmac(i,j,k+2);
+                        break;
+                    case Interior:
+                        // do nothing
+                        break;        
+                }
+            }
+
+            // hi z-faces
+            if (k == domhi[2]+2) {
+                switch (physbc_p[2+AMREX_SPACEDIM]) {
+                    case Inflow:
+                        umac(i,j,k-1) = 0.0;
+                        vmac(i,j,k-1) = 0.0;
+                        wmac(i,j,k) = wmac(i,j,k-1);
+                        break;
+                    case SlipWall:
+                    case NoSlipWall:
+                        umac(i,j,k-1) = 0.0;
+                        vmac(i,j,k-1) = 0.0;
+                        wmac(i,j,k) = 0.0;
+                        break;
+                    case Outflow:
+                        umac(i,j,k-1) = umac(i,j,k-2);
+                        vmac(i,j,k-1) = vmac(i,j,k-2);
+                        wmac(i,j,k) = wmac(i,j,k-1);
+                        break;
+                    case Symmetry:
+                        umac(i,j,k-1) = umac(i,j,k-2);
+                        vmac(i,j,k-1) = vmac(i,j,k-2);
+                        wmac(i,j,k) = -wmac(i,j,k-2);
+                        break;
+                    case Interior:
+                        // do nothing
+                        break;        
+                }
+            }
+        });
+#endif
+
+//             fill_umac_ghost(ARLIM_3D(domainBox.loVect()), ARLIM_3D(domainBox.hiVect()),
+//                             ARLIM_3D(tilebox.loVect()), ARLIM_3D(tilebox.hiVect()),
+//                             BL_TO_FORTRAN_3D(umacx_mf[mfi]),
+//                             BL_TO_FORTRAN_3D(umacy_mf[mfi]),
+// #if (AMREX_SPACEDIM == 3)
+//                             BL_TO_FORTRAN_3D(umacz_mf[mfi]),
+// #endif
+//                             phys_bc.dataPtr());
 
         }
     }
