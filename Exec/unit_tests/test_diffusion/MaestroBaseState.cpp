93edbafcfa6f3ea67d340458e7a186dc3227db57
Alice Harpole <aliceharpole@gmail.com>
2020-02-28 16:39:59 -0500

    converted base_state.F90 for test_diffusion

---
+++ Exec/unit_tests/test_diffusion/MaestroBaseState.cpp
@@ -0,0 +1,64 @@
+#include <Maestro.H>
+#include <Maestro_F.H>
+
+using namespace amrex;
+
+void 
+Maestro::InitBaseState(RealVector& s0_init, RealVector& p0_init, 
+                       RealVector& rho0, RealVector& rhoh0, 
+                       RealVector& p0, RealVector& tempbar, 
+                       RealVector& tempbar_init,
+                       const int lev)
+{
+    // timer for profiling
+    BL_PROFILE_VAR("Maestro::InitBaseState()", InitBaseState); 
+
+    // get species indices
+    const int ihe4 = network_spec_index("helium-4");
+    const int ic12 = network_spec_index("carbon-12");
+    const int ife56 = network_spec_index("iron-56");
+
+    if (ihe4 < 0 || ic12 < 0 || ife56 < 0) {
+        Print() << ihe4 << ", " << ic12 << ", " << ife56 << std::endl;
+        Abort("Invalid species in InitBaseState.");
+    }
+
+    eos_t eos_state;
+
+    eos_state.h = ambient_h;
+    eos_state.rho = ambient_dens;
+    for (auto comp = 0; comp < NumSpec; ++comp) {
+        eos_state.xn[comp] = 0.0;
+    }
+    eos_state.xn[ihe4] = ambient_he4;
+    eos_state.xn[ic12] = ambient_c12;
+    eos_state.xn[ife56] = ambient_fe56;
+
+    eos(eos_input_rh, eos_state);
+
+    Real diffusion_coefficient = const_conductivity / (eos_state.cp * ambient_dens);
+   
+    for (auto r = 0; r < nr[n]; ++r) {
+
+        s0_init[n+max_lev*(r+nr_fine*Rho)] = eos_state.rho;
+        s0_init[n+max_lev*(r+nr_fine*RhoH)] = eos_state.rho * eos_state.h;
+        for (auto comp = 0; comp < NumSpec; ++comp) {
+            s0_init[n+max_lev*(r+nr_fine*(FirstSpec+comp))] = 
+                eos_state.rho * eos_state.xn[comp];
+        }
+        p0_init[n+max_lev*r] = eos_state.p;
+        s0_init[n+max_lev*(r+nr_fine*Temp)] = eos_state.T;
+    }
+
+    // copy s0_init and p0_init into rho0, rhoh0, p0, and tempbar
+    for (auto i = 0; i < nr_fine; ++i) {
+        rho0[lev+max_lev*i] = s0_init[lev+max_lev*(i+nr_fine*Rho)];
+        rhoh0[lev+max_lev*i] = s0_init[lev+max_lev*(i+nr_fine*RhoH)];
+        tempbar[lev+max_lev*i] = s0_init[lev+max_lev*(i+nr_fine*Temp)];
+        tempbar_init[lev+max_lev*i] = s0_init[lev+max_lev*(i+nr_fine*Temp)];
+        p0[lev+max_lev*i] = p0_init[lev+max_lev*i];
+    }
+
+    // initialize any inlet BC parameters
+    SetInletBCs();
+}
