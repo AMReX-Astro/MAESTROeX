a997c1733dc604fb7926033d2400a26189ffd539
Alice Harpole <aliceharpole@gmail.com>
2020-03-24 14:17:46 -0400

    fixed overloaded operators

--- Exec/unit_tests/test_new_basestate/main.cpp
+++ Exec/unit_tests/test_new_basestate/main.cpp
@@ -10,49 +10,76 @@ int main(int argc, char* argv[])
     // in AMReX.cpp
     Initialize(argc, argv);
 
-    // // Refuse to continue if we did not provide an inputs file.
-
-    // if (argc <= 1) {
-    //     Abort("Error: no inputs file provided on command line.");
-    // }
-
-    // // Save the inputs file name for later.
-
-    // if (!strchr(argv[1], '=')) {
-    //     inputs_name = argv[1];
-    // }
-
     // timer for profiling
     BL_PROFILE_VAR("main()", main);
 
     // wallclock time
     const Real strt_total = ParallelDescriptor::second();
 
-    {
-        BaseState<Real> base_state;
+    BaseState<Real> base_state;
 
-        const int nlevs = 3;
-        const int len = 20;
-        const int ncomp = 5;
+    const int nlevs = 3;
+    const int len = 20;
+    const int ncomp = 5;
 
-        Print() << "Defining base state with " << nlevs << " levels, length " << len << " and " << ncomp << " components." << std::endl;
+    Print() << "Defining base state with " << nlevs << " levels, length " << len << " and " << ncomp << " components." << std::endl;
 
-        base_state.define(nlevs, len, ncomp);
+    base_state.define(nlevs, len, ncomp);
 
-        Print() << "setting value to 1.0" << std:: endl;
-        base_state.setVal(1.0);
+    Print() << "setting value to 1.0" << std:: endl;
+    base_state.setVal(1.0);
 
-        Print() << "print an element: " << base_state(0,0) << std::endl;
+    Print() << "print an element: " << base_state(0,0) << std::endl;
 
-        Print() << "defining another base state" << std::endl;
-        BaseState<Real> other_base_state(nlevs, len, ncomp);
+    Print() << "defining another base state" << std::endl;
+    BaseState<Real> other_base_state(nlevs, len, ncomp);
 
-        for (auto l = 0; l < nlevs; ++l) {
-            std::fill(other_base_state[l].begin(), other_base_state[l].end(), 1.0);
-        }
-
-        Print() << "are the two base states equal? " << (base_state == other_base_state) << std::endl;
+    for (auto l = 0; l < nlevs; ++l) {
+        std::fill(other_base_state[l].begin(), other_base_state[l].end(), 1.0);
     }
+
+    Print() << "are the two base states equal? " << (base_state == other_base_state) << std::endl;
+
+    Print() << "making a deep copy" << std::endl;
+
+    BaseState<Real> deep_copy(base_state);
+
+    Print() << "are the two base states equal? " << (base_state == deep_copy) << std::endl;
+
+    Print() << "Change the copy" << std::endl;
+
+    deep_copy.setVal(1, 0.0);
+
+    Print() << "are the two base states equal? " << (base_state == deep_copy) << std::endl;
+
+    Print() << "let's iterate over the base state" << std::endl;
+    for (auto l = 0; l < nlevs; ++l) {
+        AMREX_PARALLEL_FOR_1D(base_state.length(), n, {
+            base_state(l,n) = 0.5;
+        });
+    }
+
+    Print() << "we can also iterate over the components" << std::endl;
+    for (auto l = 0; l < nlevs; ++l) {
+        AMREX_PARALLEL_FOR_1D(base_state.length(), n, {
+            for (auto comp = 0; comp < base_state.nComp(); ++comp) {
+                base_state(l,n,comp) = Real(comp);
+            }
+        });
+    }
+
+
+    Print() << "multiply by scalar" << std::endl;
+
+    base_state *= 2.0;
+
+    Print() << "add two base states" << std::endl;
+
+    BaseState<Real> summed_state = base_state + other_base_state;
+
+    Print() << "subtract in place" << std::endl;
+
+    base_state -= other_base_state;
 
     // destroy timer for profiling
     BL_PROFILE_VAR_STOP(main);
