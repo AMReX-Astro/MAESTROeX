1c22f97be1ccf88fcde84aadf8a5a7f30847d6bd
Travis CI <>
2020-04-02 23:20:36 +0000

    Deploy to GitHub Pages: a8ede76b7d39d006313c3233eb695af2204af2f0

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '20.04',
+    VERSION: '20.04a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
