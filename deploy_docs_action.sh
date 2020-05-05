d0fb7f72768ba968699ca037618375c23accd6ad
Alice Harpole <harpolea@users.noreply.github.com>
2020-05-05 14:27:00 -0400

    Github action for docs build (#187)
    This switches to using a github action for the docs build (rather than having the docs built on travis). The setup for the github action is much simpler as the action does all of the work of dealing with tokens and pushing to the gh-pages branch.
    
    It also converts the tab exterminator script into a github action, and adds an action for the clang static analysis.
    
    * add github action for docs build
    
    * delete deploy_docs.sh
    
    * add tab exterminator action
    
    * only commit if there are changes so that the action doesn't fail
    
    * fix branch to push to
    
    * fix branch to push to
    
    * add cpp linter and turn off travis
    
    * add amrex as a submodule
    
    * add amrex as a submodule

---
+++ deploy_docs_action.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e # Exit with nonzero exit code if anything fails
+
+# Build the documentation from the MASTER_BRANCH or DEV_BRANCH
+# and push it to TARGET_BRANCH.
+MASTER_BRANCH="master"
+DEV_BRANCH="development"
+TARGET_BRANCH="gh-pages"
+
+mkdir out
+
+# if on the dev branch, use the dev_layout.html template to get the 
+# links correct
+if [ "$GITHUB_BRANCH" = "$DEV_BRANCH" ]; then
+    mv sphinx_docs/source/_templates/dev_layout.html sphinx_docs/source/_templates/layout.html 
+fi
+
+# Build the Sphinx documentation
+cd sphinx_docs
+make html
+cd ../
+
+mkdir -p out/docs/
+if [ "$GITHUB_BRANCH" = "$MASTER_BRANCH" ]; then
+    mv sphinx_docs/build/html/* out/docs
+else 
+    mkdir -p out/docs/dev/
+    mv sphinx_docs/build/html/* out/docs/dev
+fi
