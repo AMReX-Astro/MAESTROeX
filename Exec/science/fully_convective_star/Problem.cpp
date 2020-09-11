492d9e1c7f17a82203ff4f77b21b46b35d68d9da
GitHub Action <action@github.com>
2020-09-11 02:00:20 +0000

    Clang-format has tidied up the code.

--- Exec/science/fully_convective_star/Problem.cpp
+++ Exec/science/fully_convective_star/Problem.cpp
@@ -234,10 +234,10 @@ void Maestro::MakeConvectionVel(const Vector<MultiFab>& velr,
     for (auto r = 0; r < base_geom.nr_fine; ++r) {
         // DEBUG
         // if (vel_conv_arr(0, r) < 0)
-	//     Print() << "HACK: " << r << ", " << vel_conv_arr(0, r) << std::endl;
+        //     Print() << "HACK: " << r << ", " << vel_conv_arr(0, r) << std::endl;
 
-	// Note that the abs value is used here because the Average() subroutine
-	// causes some interpolation values near center of star to be negative.
+        // Note that the abs value is used here because the Average() subroutine
+        // causes some interpolation values near center of star to be negative.
         // This should not affect values located further from the center.
         totsum_arr(0, r, comp) += dt * amrex::Math::abs(vel_conv_arr(0, r));
 
