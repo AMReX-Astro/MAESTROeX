24192eebfdf4b88fdd41639202cf87c38ff715ce
Doreen Fan <dfan@lbl.gov>
2021-09-13 15:29:05 -0700

    Add Burner subroutine that uses ML model

--- Source/MaestroBurner.cpp
+++ Source/MaestroBurner.cpp
@@ -5,6 +5,227 @@
 using namespace amrex;
 
 #ifndef SDC
+
+#ifdef ML
+
+// Use ML model to replace Microphysics solver
+void Maestro::Burner(const Vector<MultiFab>& s_in, Vector<MultiFab>& s_out,
+                     const Vector<MultiFab>& rho_Hext,
+                     Vector<MultiFab>& rho_omegadot, Vector<MultiFab>& rho_Hnuc,
+                     const BaseState<Real>& p0, const Real dt_in,
+                     const Real time_in) {
+    // timer for profiling
+    BL_PROFILE_VAR("Maestro::Burner()", Burner);
+
+    // Put tempbar_init on cart
+    Vector<MultiFab> tempbar_init_cart(finest_level + 1);
+
+    if (spherical) {
+        for (int lev = 0; lev <= finest_level; ++lev) {
+            tempbar_init_cart[lev].define(grids[lev], dmap[lev], 1, 0);
+            tempbar_init_cart[lev].setVal(0.);
+        }
+
+        if (drive_initial_convection) {
+            Put1dArrayOnCart(tempbar_init, tempbar_init_cart, false, false,
+                             bcs_f, 0);
+        }
+    }
+
+    const auto ispec_threshold = network_spec_index(burner_threshold_species);
+
+    // data type of pytorch tensor
+    auto dtype0 = torch::kFloat64;
+    
+    for (int lev = 0; lev <= finest_level; ++lev) {
+        // create mask assuming refinement ratio = 2
+        int finelev = lev + 1;
+        if (lev == finest_level) finelev = finest_level;
+
+        const BoxArray& fba = s_in[finelev].boxArray();
+        const iMultiFab& mask = makeFineMask(s_in[lev], fba, IntVect(2));
+
+        // loop over boxes (make sure mfi takes a cell-centered multifab as an argument)
+#ifdef _OPENMP
+#pragma omp parallel
+#endif
+        for (MFIter mfi(s_in[lev], TilingIfNotGPU()); mfi.isValid(); ++mfi) {
+            // Get the index space of the valid region
+            const Box& tileBox = mfi.tilebox();
+
+            const bool use_mask = (lev != finest_level);
+
+            const Array4<const Real> s_in_arr = s_in[lev].array(mfi);
+            const Array4<Real> s_out_arr = s_out[lev].array(mfi);
+            const Array4<const Real> rho_Hext_arr = rho_Hext[lev].array(mfi);
+            const Array4<Real> rho_omegadot_arr = rho_omegadot[lev].array(mfi);
+            const Array4<Real> rho_Hnuc_arr = rho_Hnuc[lev].array(mfi);
+            const auto tempbar_init_arr = tempbar_init.const_array();
+	    
+            // use a dummy value for non-spherical as tempbar_init_cart is not defined
+            const Array4<const Real> tempbar_cart_arr =
+                spherical ? tempbar_init_cart[lev].array(mfi)
+                          : rho_Hext[lev].array(mfi);
+	    
+            const Array4<const int> mask_arr = mask.array(mfi);
+
+	    // retrieve smallend and size of box
+	    const IntVect bx_lo = tileBox.smallEnd();
+	    const IntVect nbox = tileBox.size();
+
+	    // compute total cells in the box
+	    int ncell = AMREX_SPACEDIM == 2 ?
+		nbox[0] * nbox[1] : nbox[0] * nbox[1] * nbox[2];
+	    
+	    // copy multifabs to input array
+	    // index ordering: (dt, species, rho, temp)
+	    const int NumInput = NumSpec + 3;
+	    amrex::Gpu::ManagedVector<Real> state_temp(ncell*NumInput);
+	    Real* AMREX_RESTRICT temp_ptr = state_temp.dataPtr();
+
+	    // copy input multifab to torch tensor
+	    amrex::ParallelFor(tileBox, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                int ii = i - bx_lo[0];
+		int jj = j - bx_lo[1];
+		int index = jj*nbox[0] + ii;
+#if AMREX_SPACEDIM == 3
+		int kk = k - bx_lo[2];
+		index += kk*nbox[0]*nbox[1];
+#endif
+		
+		auto rho = s_in_arr(i, j, k, Rho);
+		Real T_in = 0.0;
+                if (drive_initial_convection) {
+                    if (!spherical) {
+                        auto r = (AMREX_SPACEDIM == 2) ? j : k;
+                        T_in = tempbar_init_arr(lev, r);
+                    } else {
+                        T_in = tempbar_cart_arr(i, j, k);
+                    }
+                } else {
+                    T_in = s_in_arr(i, j, k, Temp);
+                }
+                
+		// array order is row-based [index][comp]
+		temp_ptr[index*NumInput] = dt_in;
+		for (int n = 0; n < NumSpec; ++n) {
+		    temp_ptr[index*NumInput + 1 + n] = s_in_arr(i, j, k, FirstSpec + n) / rho;
+		}
+		temp_ptr[index*NumInput + NumSpec + 1] = rho;
+		temp_ptr[index*NumInput + NumSpec + 2] = T_in;
+	    });
+
+	    // create torch tensor from array
+#ifndef AMREX_USE_CUDA
+	    at::Tensor inputs_torch = torch::from_blob(temp_ptr, {ncell, NumInput},
+						       torch::TensorOptions().dtype(dtype0));
+#else
+	    at::Tensor inputs_torch = torch::from_blob(temp_ptr, {ncell, NumInput},
+						       torch::TensorOptions().dtype(dtype0).device(torch::kCUDA));
+#endif
+
+	    // evaluate torch model
+	    at::Tensor outputs_torch = module.forward({inputs_torch}).toTensor();
+	    outputs_torch = outputs_torch.to(dtype0);
+
+	    // get accessor to tensor (read-only)
+#ifndef AMREX_USE_CUDA
+	    auto outputs_torch_acc = outputs_torch.accessor<Real,2>();
+#else
+	    auto outputs_torch_acc = outputs_torch.packed_accessor64<Real,2>();
+#endif
+
+	    // copy output tensor to multifabs
+	    // index ordering: (species, enuc)
+            ParallelFor(tileBox, [=] AMREX_GPU_DEVICE(int i, int j, int k) noexcept {
+                if (use_mask && mask_arr(i, j, k))
+                    return;  // cell is covered by finer cells
+
+		int ii = i - bx_lo[0];
+		int jj = j - bx_lo[1];
+		int index = jj*nbox[0] + ii;
+#if AMREX_SPACEDIM == 3
+		int kk = k - bx_lo[2];
+		index += kk*nbox[0]*nbox[1];
+#endif
+
+		auto rho = temp_ptr[index*NumInput + NumSpec + 1];
+		Real x_in[NumSpec];
+		for (int n = 0; n < NumSpec; ++n) {
+		    x_in[n] = temp_ptr[index*NumInput + 1 + n];
+		}
+		
+                Real x_test =
+                    (ispec_threshold > 0) ? x_in[ispec_threshold] : 0.0;
+
+		Real x_out[NumSpec];
+                Real rhowdot[NumSpec];
+                Real rhoH = 0.0;
+
+                // if the threshold species is not in the network, then we burn
+                // normally.  if it is in the network, make sure the mass
+                // fraction is above the cutoff.
+                if ((rho > burning_cutoff_density_lo &&
+                     rho < burning_cutoff_density_hi) &&
+                    (ispec_threshold < 0 ||
+                     (ispec_threshold > 0 &&
+                      x_test > burner_threshold_cutoff))) {
+
+                    for (int n = 0; n < NumSpec; ++n) {
+                        x_out[n] = outputs_torch_acc[index][n];
+                        rhowdot[n] = rho * (x_out[n] - x_in[n]) / dt_in;
+                    }
+
+		    // note enuc in output tensor is the same as (state_out.e - state_in.e)
+                    rhoH = rho * outputs_torch_acc[index][NumSpec] / dt_in;
+                } else {
+                    for (int n = 0; n < NumSpec; ++n) {
+                        x_out[n] = x_in[n];
+                        rhowdot[n] = 0.0;
+                    }
+                }
+
+                // check if sum{X_k} = 1
+                Real sumX = 0.0;
+                for (int n = 0; n < NumSpec; ++n) {
+                    sumX += x_out[n];
+                }
+
+                if (fabs(sumX - 1.0) > reaction_sum_tol) {
+#ifndef AMREX_USE_GPU
+                    Print() << "WARNING: abundances do not sum to 1" << std::endl;
+#endif
+                    for (int n = 0; n < NumSpec; ++n) {
+                        x_out[n] /= sumX;
+                    }
+                }
+
+                // pass the density and pi through
+                s_out_arr(i, j, k, Rho) = s_in_arr(i, j, k, Rho);
+                s_out_arr(i, j, k, Pi) = s_in_arr(i, j, k, Pi);
+
+                // update the species
+                for (int n = 0; n < NumSpec; ++n) {
+                    s_out_arr(i, j, k, FirstSpec + n) = x_out[n] * rho;
+                }
+
+                // store the energy generation and species create quantities
+                for (int n = 0; n < NumSpec; ++n) {
+                    rho_omegadot_arr(i, j, k, n) = rhowdot[n];
+                }
+                rho_Hnuc_arr(i, j, k) = rhoH;
+
+                // update the enthalpy -- include the change due to external heating
+                s_out_arr(i, j, k, RhoH) = s_in_arr(i, j, k, RhoH) +
+                                           dt_in * rho_Hnuc_arr(i, j, k) +
+                                           dt_in * rho_Hext_arr(i, j, k);
+            });
+        }
+    }
+}
+
+#else
+
 void Maestro::Burner(const Vector<MultiFab>& s_in, Vector<MultiFab>& s_out,
                      const Vector<MultiFab>& rho_Hext,
                      Vector<MultiFab>& rho_omegadot, Vector<MultiFab>& rho_Hnuc,
@@ -207,7 +428,10 @@ void Maestro::Burner(const Vector<MultiFab>& s_in, Vector<MultiFab>& s_out,
     }
 }
 
+#endif // ML
+
 #else
+
 // SDC burner
 void Maestro::Burner(const Vector<MultiFab>& s_in, Vector<MultiFab>& s_out,
                      const BaseState<Real>& p0, const Real dt_in,
@@ -383,4 +607,5 @@ void Maestro::Burner(const Vector<MultiFab>& s_in, Vector<MultiFab>& s_out,
         }
     }
 }
-#endif
+
+#endif // SDC
