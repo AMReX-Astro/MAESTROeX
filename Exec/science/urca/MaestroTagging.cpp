f2c3b3ad27517afb2a9cd775199047bc47c85f5b
Michael Zingale <michael.zingale@stonybrook.edu>
2021-11-05 19:08:58 -0400

    fix tileBox -> tilebox (#279)

--- Exec/science/urca/MaestroTagging.cpp
+++ Exec/science/urca/MaestroTagging.cpp
@@ -31,7 +31,7 @@ void Maestro::StateError(TagBoxArray& tags, const MultiFab& state_mf,
     const Box& tilebox = mfi.tilebox();
 
     // Tag on regions of high density
-    ParallelFor(tileBox, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
+    ParallelFor(tilebox, [=] AMREX_GPU_DEVICE(int i, int j, int k) {
         if ((lev == 0 && state(i, j, k, Rho) >= tag_density_1) ||
             (lev == 1 && state(i, j, k, Rho) >= tag_density_2) ||
             (lev == 2 && state(i, j, k, Rho) >= tag_density_3)) {
