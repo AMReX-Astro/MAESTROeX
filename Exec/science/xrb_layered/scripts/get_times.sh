3327f905b27419816c81b2dbe9bc1f88438d1c17
Simon Guichandut <simonguichandut@gmail.com>
2023-06-01 09:24:18 -0700

    some changes

--- Exec/science/xrb_layered/scripts/get_times.sh
+++ Exec/science/xrb_layered/scripts/get_times.sh
@@ -3,5 +3,5 @@ RUN_DIR=$1
 PLOT_DIR=PLOTS
 PLOT_BASENAME="xrb_0"
 
-faverage=$AMREX_HOME/Tools/Plotfile/ftime.gnu.x86-milan.ex
-$faverage $RUN_DIR/$PLOT_DIR/$PLOT_BASENAME* > $RUN_DIR/"times.txt"
+ftime=$AMREX_HOME/Tools/Plotfile/ftime.gnu.x86-milan.ex
+$ftime $RUN_DIR/$PLOT_DIR/$PLOT_BASENAME* > $RUN_DIR/"times.txt"
