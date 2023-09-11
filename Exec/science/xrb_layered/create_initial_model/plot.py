6e9cd2cc43fb989fa84de987fec90aa41e9887b4
Simon Guichandut <simonguichandut@gmail.com>
2023-09-11 17:34:34 -0400

    new initial model, with dF/dy=-eps, and X=X(y)

--- Exec/science/xrb_layered/create_initial_model/plot.py
+++ Exec/science/xrb_layered/create_initial_model/plot.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 
 import mesa.py_mesa_reader as mr
 
+import sys
+filename = sys.argv[1]
+
 fig,axes = plt.subplots(3,1,figsize=(6,8), sharex=True)
 fig.subplots_adjust(hspace=0)
 
@@ -27,7 +30,7 @@ axes[2].semilogy(prof.R_cm-12e5, cno, 'g-',label="CNO")
 axes[2].semilogy(prof.R_cm-12e5, prof.fe56, 'k-',label="fe56")
 
 # toy atm model
-all_data = np.loadtxt("toy_atm/toy_atm_3cm.hse", skiprows=26).T
+all_data = np.loadtxt(filename, skiprows=26).T
 names = ["density","temperature","pressure","hydrogen-1","helium-4","carbon-12","carbon-13","nitrogen-13","nitrogen-14","nitrogen-15","oxygen-14","oxygen-15","oxygen-16","oxygen-17","oxygen-18","fluorine-17","fluorine-18","fluorine-19","neon-18","neon-19","neon-20","magnesium-22","magnesium-24","iron-56"]
 data = {name:all_data[i+1] for i,name in enumerate(names)}
 z = all_data[0]
@@ -43,4 +46,4 @@ axes[2].semilogy(z, data["iron-56"], 'k--')
 axes[0].legend(frameon=False, ncol=2, bbox_to_anchor = (0.8,1.15), bbox_transform=axes[0].transAxes)
 axes[2].legend(loc=3)
 
-fig.savefig("toy_atm.png", bbox_inches="tight", dpi=500)
+fig.savefig(filename.split("/")[-1] + ".png", bbox_inches="tight", dpi=500)
